9bc08779dff81c44cd03d5123c8ae2b9eccb6bcc
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2022-02-16 20:09:03 -0500

    finished comment routes and the ability to add and edit comments

--- models/Comment.js
+++ models/Comment.js
@@ -1,7 +1,36 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReplySchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment _id
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        }
+    }
+);
 
 const CommentSchema = new Schema({
-    writtenBy: {
+    
+        writtenBy: {
         type: String
     },
     commentBody: {
@@ -9,8 +38,23 @@ const CommentSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
-    }
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    // use replies schema to validate data for a reply
+    replies: [ReplySchema],
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+    id: false
+}
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
 });
 
 const Comment = model('Comment', CommentSchema);
